ec4af989e30fe8f9378113d4b86053052cc85c01
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2019-11-25 19:24:51 +0100

    Final commit

--- Ejercicio 2.s
+++ Ejercicio 2.s
@@ -1,43 +1,41 @@
-# A = R4 = $4
+# A = R4 = A
 # BC = R5
 # DE = R6
 # HL = R7 = $v0
 # IX = R8 = $a0
 # IY = R9 = $a1
 
-
-
 .data
 	vector: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
 .text
 	sumav:
-		push $8		#Push $a0
-		push $9		#Push $a1
+		push IX		#Push $a0
+		push IY		#Push $a1
 
-		ldi $7 0	#li $v0
+		ldi HL 0	#li $v0
 
 		b1:
-			ldi $4 0	#ponemos A a 0
-			add_a $8	#A + v0
-			jpz 36		#PREGUNTAR EN CLASE SI ES LEGITIMO, comprobar si la suma es 0
-			ld $5 ($9)	#ponemos R5 (PREGUNTAR SI R5 ES $t0) al valor de la posicion en memoria de R9
-			ld $4 $7	#movemos a A v0
-			add_a $5	#A + R5
-			ld $7 $4 	#movemos a v0 A
-			ld $4 $9	#movemos a A a1
+			ldi A 0	#ponemos A a 0
+			add_a IX	#A + v0
+			jpz f1		#PREGUNTAR EN CLASE SI ES LEGITIMO, comprobar si la suma es 0
+			ld BC (IY)	#ponemos R5 (PREGUNTAR SI R5 ES $t0) al valor de la posicion en memoria de R9
+			ld A HL	#movemos a A v0
+			add_a BC	#A + R5
+			ld HL A 	#movemos a v0 A
+			ld A IY	#movemos a A a1
 			addi_a 4	#A + 4
-			ld $9 $4	#movemos a a1 A
-			dec $8		#a0 - 1
-			jp -48		#reiterar
+			ld IY A	#movemos a a1 A
+			dec IX		#a0 - 1
+			jp b1		#reiterar
 		f1:
-			pop $9		#Pop $a1
-			pop $8		#Pop $a0
+			pop IY		#Pop $a1
+			pop IX		#Pop $a0
 			ret 		#RETURN
 
 	main:
-		ldi $8 10
-		ldi $9 vector
+		ldi IX 10
+		ldi IY vector
 		call sumav
 		halt
 		
